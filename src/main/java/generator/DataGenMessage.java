67886d5db0046a6f1e6d1f74b0116d1297959a27
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-10-15 20:41:32 +0000

    Remove build, add timestamp, company and region

--- src/main/java/generator/DataGenMessage.java
+++ src/main/java/generator/DataGenMessage.java
@@ -2,6 +2,9 @@ package generator;
 
 import java.util.UUID;
 import java.util.Random;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by dtregonning on 9/12/17.
@@ -10,11 +13,13 @@ public class DataGenMessage {
     String message = null;
     UUID uuid = null;
     int sequenceNumber = 0;
+    Timestamp timestamp = null;
 
     public DataGenMessage(int size, int sequenceNumber) {
         this.uuid = UUID.randomUUID();
         this.message = String.valueOf(randomizeMessage(size));
         this.sequenceNumber = sequenceNumber;
+	this.timestamp = new Timestamp(System.currentTimeMillis());
     }
 
     private char[] randomizeMessage(int size) {
@@ -31,6 +36,6 @@ public class DataGenMessage {
     }
 
     public String toJSON() {
-        return "{"+ "\"VSAD_ID\":\"acme\", " + "\"eventKey\":\"" + this.sequenceNumber + "\", \"uuid\":\"" + this.uuid +"\", \"message\":\"" + this.message + "\"}";
+        return "{"+ "\"timestamp\":\"" + this.timestamp + "\", \"region\": \"EMEA\", \"company\":\"acme\", " + "\"eventKey\":\"" + this.sequenceNumber + "\", \"uuid\":\"" + this.uuid +"\", \"message\":\"" + this.message + "\"}";
     }
 }
